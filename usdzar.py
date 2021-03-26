f4aac2cb25d3fd5301306ca8da310773c40b3556
Grant Fairbrother <56653913+Author1t3@users.noreply.github.com>
2021-03-26 11:02:49 +0200

    Trained the model and saved the model

--- usdzar.py
+++ usdzar.py
@@ -3,13 +3,14 @@
 
 # Import the libraries
 import math
-#
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
+from keras.models import model_from_json
+import os
 plt.style.use('fivethirtyeight')
 
 # check the data
@@ -20,9 +21,79 @@ df = pd.read_csv('USDZAR60.csv')
 # print(df.shape)
 
 # Visualize the closing price
-plt.figure(figsize=(16, 9))
-plt.title('Close Price History')
-plt.plot(df['Close'])
-plt.xlabel('Date', fontsize=18)
-plt.ylabel('Close Price USDZAR', fontsize=18)
-plt.show()
+# plt.figure(figsize=(16, 9))
+# plt.title('Close Price History')
+# plt.plot(df['Close'])
+# plt.xlabel('Date', fontsize=18)
+# plt.ylabel('Close Price USDZAR', fontsize=18)
+# plt.show()
+
+# Create a new dataframe with only the Close column
+data = df.filter(['Close'])
+# Convert the dataframe to a numpy array
+dataset = data.values
+# get the number of rows to train the model on
+training_data_len = math.ceil(len(dataset) * .8)
+
+# print(training_data_len)
+
+# Scale the data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
+
+# print(scaled_data)
+
+# Create the training data set
+# Create the scaled training data set
+train_data = scaled_data[0:training_data_len, :]
+
+# Split the data into x_train and y_train data sets
+x_train = []
+y_train = []
+
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+
+    # if i <= 61:
+    #     print(x_train)
+    #     print(y_train)
+    #     print()
+
+# Convert the x_train and y_train to numpy arrays
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+# Reshape the data
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+# print(x_train.shape)
+
+# Build the LSTM model
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(50, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the model
+#model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+# Save the model
+# model.save('model.h5')
+
+# Load the model
+model = keras.models.load_model('model.h5')
+
+# Create the testing data set
+# Create a new array containing scaled values from training_data_len to end
+
+test_data = scaled_data[training_data_len - 60:, :]
+
+# Create the data sets x_test and y_test
+x_test = []
+y_test = dataset[training_data_len:, :]
+
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
